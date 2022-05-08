723c565fcfd71fa7cc8a921565b54ac4444f3187
Fridah-kalee <fridahkalee@gmail.com>
2022-05-08 12:25:42 +0300

    fixing changes

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from . import main
+from ..models import Pitch
 
 
 @main.route('/')
@@ -7,10 +8,15 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
+    # Getting reviews by category
+    interview_pitches = Pitch.get_pitches('interview')
+    product_pitches = Pitch.get_pitches('product')
+    promotion_pitches = Pitch.get_pitches('promotion')
+
    
     
     
     title='Welcome to pitches website'
 
-    return render_template('index.html',title=title)
+    return render_template('index.html',title=title,interview=interview_pitches,product=product_pitches,promotion=promotion_pitches)
 
--- config.py
+++ config.py
@@ -5,6 +5,7 @@ class Config:
     '''
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:kalee@localhost/pitches'
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 class ProdConfig(Config):
     '''
