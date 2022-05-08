bbdd740c24b5c096f19ebc5ed36930d993b11994
Fridah-kalee <fridahkalee@gmail.com>
2022-05-08 11:32:55 +0300

    created database

--- app/main/views.py
+++ app/main/views.py
@@ -1,10 +1,16 @@
 from flask import render_template
 from . import main
 
+
 @main.route('/')
 def index():
     '''
     View root page function that returns the index page and its data
     '''
+   
+    
+    
     title='Welcome to pitches website'
+
     return render_template('index.html',title=title)
+
