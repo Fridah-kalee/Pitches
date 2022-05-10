89af86dc5b449973aa3183916d498eb762f13507
Fridah-kalee <fridahkalee@gmail.com>
2022-05-10 11:41:04 +0300

    add file to tests folder

--- manage.py
+++ manage.py
@@ -1,16 +1,18 @@
 from app import create_app,db
 from flask_script import Manager,Server
-from app.models import Pitch
+from app.models import User,Pitch
+from  flask_migrate import Migrate
 
 # creating app instance
 app = create_app('development')
 
 manager = Manager(app)
 manager.add_command('server',Server)
+migrate = Migrate(app,db)
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,Pitch = Pitch )
+    return dict(app = app,db = db,User=user,Pitch = Pitch )
 
 
 if __name__=='__main__':
