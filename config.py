b25a49907b261a0d25b16fa7c80d3c5821b74eb6
Fridah-kalee <fridahkalee@gmail.com>
2022-05-13 13:27:13 +0300

    changing flask migrate version

--- config.py
+++ config.py
@@ -19,12 +19,7 @@ class ProdConfig(Config):
     Config: The parent configuration class with General configuration settings
     '''
     # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://oytwsfvqjadzgf:74e4dd3c6e4d7fd980643b681d9145baf3b98611eafcd639a33d8941229f67ab@ec2-3-217-113-25.compute-1.amazonaws.com:5432/d89nv1bn48c0fa'
-    # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL").replace("postgres://", "postgresql://",1)
-    uri = os.environ.get("DATABASE_URL")  # or other relevant config var
-    if uri.startswith("postgres://"):
-        uri = uri.replace("postgres://", "postgresql://", 1)
-    # rest of connection code using the connection string `uri`
-    SQLALCHEMY_DATABASE_URI = uri
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL").replace("postgres://", "postgresql://",1)
 
 class TestConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:kalee@localhost/pitches_test'
